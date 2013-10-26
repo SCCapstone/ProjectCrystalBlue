f4d1656511b358ef5c1989e8c9a2c2ca7b9ce1c4
hoodlm <hoodlma@gmail.com>
2013-10-26 12:06:05 -0400

    Added blank cocoa project to repo

---
+++ ProjectCrystalBlueOSX/ProjectCrystalBlueOSX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProjectCrystalBlueOSX
+//
+//  Created by Logan Hood on 10/26/13.
+//  Copyright (c) 2013 Logan Hood. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ProjectCrystalBlueOSX/ProjectCrystalBlueOSX/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  ProjectCrystalBlueOSX
+//
+//  Created by Logan Hood on 10/26/13.
+//  Copyright (c) 2013 Logan Hood. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
