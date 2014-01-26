41b7e1479a83f0db1b0fa518e70dbaabc49d8ad6
hoodlm <hoodlma@gmail.com>
2014-01-25 22:23:33 -0500

    Added some initialization code to S3ImageStore to test that the client is being initialized and can perform basic operation (checking for & creating buckets)

--- ProjectCrystalBlueOSX/ProjectCrystalBlueOSX/S3ImageStore.m
+++ ProjectCrystalBlueOSX/ProjectCrystalBlueOSX/S3ImageStore.m
@@ -9,13 +9,46 @@
 //
 
 #import "S3ImageStore.h"
+#import <AWSiOSSDK/S3/AmazonS3Client.h>
+
+NSString *const CLASS_NAME = @"S3ImageStore";
+NSString *const BUCKET = @"project-crystal-blue-test";
 
 BOOL singletonInstantiated = NO;
+AmazonS3Client *s3Client;
 
 @implementation S3ImageStore
 
 +(void)setUpSingleton
 {
+    if (singletonInstantiated) {
+        return;
+    }
+    
+    // TEMPORARILY HARD-CODING CREDENTIALS for test purposes.
+    // This is obviously a huge security issue and cannot be in the Beta version.
+    AmazonCredentials *login =
+    [[AmazonCredentials alloc] initWithAccessKey:@"AKIAIAWCA532UPYBPVAA"
+                                   withSecretKey:@"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"];
+    
+    s3Client = [[AmazonS3Client alloc] initWithCredentials:login];
+    
+    // Next, check if our bucket exists
+    NSArray *buckets = [s3Client listBuckets];
+    NSLog(@"%@: Found %lu buckets", CLASS_NAME, (unsigned long)[buckets count]);
+    
+    bool bucketExists = NO;
+    for (S3Bucket *bucket in buckets) {
+        if ([[bucket name] isEqualToString:BUCKET]) {
+            bucketExists = YES;
+            break;
+        }
+    }
+    if (!bucketExists) {
+        NSLog(@"%@: Creating bucket %@", CLASS_NAME, BUCKET);
+        [s3Client createBucketWithName:BUCKET];
+    }
+    
     singletonInstantiated = YES;
 }
 
