e11518dbec30d089ad3e038f2af7d03d38ec511b
hoodlm <hoodlma@gmail.com>
2014-01-31 18:06:17 -0500

    Removed extraneous information from the S3ImageStore and LocalImageStore headers

--- ProjectCrystalBlueOSX/ProjectCrystalBlueOSX/LocalImageStore.h
+++ ProjectCrystalBlueOSX/ProjectCrystalBlueOSX/LocalImageStore.h
@@ -16,42 +16,4 @@
     NSString *localDirectory;
 }
 
--(id)initWithLocalDirectory:(NSString *)directory;
-
-/** Retrieve the image associated with a given key.
- */
--(NSImage *)getImageForKey:(NSString *)key;
-
-/** Checks if the LocalImageStore already has an image for the given key.
- *  For example, this should always be used before assigning a key to a new image.
- */
--(BOOL)imageExistsForKey:(NSString *)key;
-
-/** Add a new image to the LocalImageStore with the given unique key.
- *
- *  The key absolutely positively *MUST* be unique across all devices.
- *
- *  Returns YES if the put operation is successful; NO if it is unsuccessful.
- *  Generally, the only reason this should be unsuccessful is if the key is not unique
- *  or the device disk cannot be written to.
- *
- *  This only guarantees that the image has been added to a LOCAL image storage; not necessarily
- *  to the cloud storage location.
- */
--(BOOL)putImage:(NSImage *)image
-         forKey:(NSString *)key;
-
-/** Removes an image from local storage. This is mostly for testing purposes - we probably should never need
- *  to remove images from the ImageStore. This could cause major issues if an image is deleted locally but
- *  still exists on S3.
- */
--(BOOL)deleteImageWithKey:(NSString *)key;
-
-/** This will delete ALL local image data. No images are available until resynchronizing with the online database.
- *
- *  DO NOT CALL THIS METHOD LIGHTLY.
- *  Resyncing the images will likely take a lot of time. (and will certainly eat up a lot of bandwidth)
- */
--(void)flushLocalImageStore;
-
 @end
