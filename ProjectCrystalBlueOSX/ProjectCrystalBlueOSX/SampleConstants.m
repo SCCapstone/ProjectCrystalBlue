b7eee323b84160da96fe139296110fc6f8f9d69d
Ryan McGraw <r.mcgraw2010@gmail.com>
2014-02-01 17:52:49 -0500

    Added the source/sample attributes/default values from the iOS changes

--- ProjectCrystalBlueOSX/ProjectCrystalBlueOSX/SampleConstants.m
+++ ProjectCrystalBlueOSX/ProjectCrystalBlueOSX/SampleConstants.m
@@ -1,6 +1,6 @@
 //
 //  SampleConstants.m
-//  ProjectCrystalBlueOSX
+//  ProjectCrystalBlue-iOS
 //
 //  Created by Justin Baumgartner on 1/18/14.
 //  Copyright (c) 2014 Project Crystal Blue. All rights reserved.
@@ -11,10 +11,37 @@
 /* Attribute names
  */
 NSString *const CURRENT_LOCATION = @"CurrentLocation";
+NSString *const BULK_ROCK = @"Bulk_Rock";
+NSString *const RAINY_DAY = @"Rainy_Day";
+NSString *const HAND_SAMPLE = @"Hand_Sample";
+NSString *const DEEP_STORAGE = @"Deep_Storage";
+NSString *const ROCK_SAW = @"Rock_Saw";
+NSString *const JAW_CRUSHER = @"Jaw_Crusher";
+NSString *const PULVERIZER = @"Pulverizer";
+NSString *const SIEVES = @"Sieves";
+NSString *const GEMENI = @"Gemeni";
+NSString *const GOLD_PAN = @"Gold_Pan";
+NSString *const HEAVY_LIQUID = @"Heavy_Liquid";
+NSString *const MAGNETIC_SEPERATOR = @"Magnetic_Seperator";
+NSString *const TAGS = @"Tags";
+
 
 /* Attribute default values
  */
 NSString *const DEF_VAL_CURRENT_LOCATION = @"USC";
+NSString *const DEF_VAL_BULK_ROCK = @"Is Bulk_Rock";
+NSString *const DEF_VAL_RAINY_DAY = @"Not Rainy_Day";
+NSString *const DEF_VAL_HAND_SAMPLE = @"Not Hand_Sample";
+NSString *const DEF_VAL_DEEP_STORAGE = @"Not Deep_Storage";
+NSString *const DEF_VAL_ROCK_SAW = @"Not Rock_Saw";
+NSString *const DEF_VAL_JAW_CRUSHER = @"Not Jaw_Crusher";
+NSString *const DEF_VAL_PULVERIZER = @"Not Pulverizer";
+NSString *const DEF_VAL_SIEVES = @"Not Sieves";
+NSString *const DEF_VAL_GEMENI = @"Not Gemeni";
+NSString *const DEF_VAL_GOLD_PAN = @"Not Gold_Pan";
+NSString *const DEF_VAL_HEAVY_LIQUID = @"Not Heavy_Liquid";
+NSString *const DEF_VAL_MAGNETIC_SEPERATOR = @"Not Magnetic_Seperator";
+NSString *const DEF_VAL_TAGS = @"";
 
 @implementation SampleConstants
 
@@ -24,8 +51,7 @@ NSString *const DEF_VAL_CURRENT_LOCATION = @"USC";
     if (!attributeNames)
     {
         attributeNames = [NSArray arrayWithObjects:
-                          CURRENT_LOCATION,
-                          nil];
+                          CURRENT_LOCATION, BULK_ROCK, RAINY_DAY, HAND_SAMPLE, DEEP_STORAGE, ROCK_SAW, JAW_CRUSHER, PULVERIZER, SIEVES, GEMENI, GOLD_PAN, HEAVY_LIQUID, MAGNETIC_SEPERATOR, TAGS, nil];
     }
     return attributeNames;
 }
@@ -36,7 +62,7 @@ NSString *const DEF_VAL_CURRENT_LOCATION = @"USC";
     if (!attributeDefaultValues)
     {
         attributeDefaultValues = [NSArray arrayWithObjects:
-                                  DEF_VAL_CURRENT_LOCATION,
+                                  DEF_VAL_CURRENT_LOCATION, DEF_VAL_BULK_ROCK, DEF_VAL_RAINY_DAY, DEF_VAL_HAND_SAMPLE, DEF_VAL_DEEP_STORAGE, DEF_VAL_ROCK_SAW, DEF_VAL_JAW_CRUSHER, DEF_VAL_PULVERIZER, DEF_VAL_SIEVES, DEF_VAL_GEMENI, DEF_VAL_GOLD_PAN, DEF_VAL_HEAVY_LIQUID, DEF_VAL_MAGNETIC_SEPERATOR, DEF_VAL_TAGS,
                                   nil];
     }
     return attributeDefaultValues;
