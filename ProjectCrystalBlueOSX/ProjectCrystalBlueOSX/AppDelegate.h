0cb621c2eb13151a80aef19a816f445897632a2c
hoodlm <hoodlma@gmail.com>
2014-02-08 00:48:57 -0500

    initialized project

---
+++ ProjectCrystalBlueOSX/ProjectCrystalBlueOSX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProjectCrystalBlueOSX
+//
+//  Created by Logan Hood on 2/8/14.
+//  Copyright (c) 2014 Project Crystal Blue. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ProjectCrystalBlueOSX/ProjectCrystalBlueOSX/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  ProjectCrystalBlueOSX
+//
+//  Created by Logan Hood on 2/8/14.
+//  Copyright (c) 2014 Project Crystal Blue. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
